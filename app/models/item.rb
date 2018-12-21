e0cade4a5cb5c12bc542e8241b69b208ac4460ee
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-21 10:58:26 -0700

    Makes final commit. Part way through User Story 2

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   validates_presence_of :name, :price
 
   has_many :machines
+
+  # def self.average_price
+  #    average(SELECT price FROM items.where machine_id: id)
+  # end
 end
--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -25,4 +25,24 @@ feature 'When a user visits a vending machine show page' do
     expect(page).to have_content("Name: #{item_2.name}")
     expect(page).to have_content("Price: $#{item_2.price}")
   end
+
+  it 'has an average price for all snacks in that machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons_machine  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    item_1 = dons_machine.items.create(name: "Snack-Name 1", price: 1)
+    item_2 = dons_machine.items.create(name: "Snack-Name 2", price: 2)
+
+    visit machine_path(dons_machine)
+
+    expect(page).to have_content("Average Price: $#{1.50}")
+  end
 end
+
+# User Story 2 of 3
+#
+# As a visitor
+# When I visit a vending machine show page
+# >>>>>>>>>>>>>>>>>>>>
+
+# I also see an average price for all of the snacks in that machine
